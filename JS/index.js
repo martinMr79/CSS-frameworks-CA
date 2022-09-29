7283f9d547a12e6af633c565de8fb1b6dfde5fed
Martin Mroz <80599295+martinMr79@users.noreply.github.com>
2022-09-29 12:22:30 +0200

    trying to add Post data

--- JS/index.js
+++ JS/index.js
@@ -1,9 +1,14 @@
+/*API Call*/
+
 const apiUrl = "https://nf-api.onrender.com/api/v1/books"
 
 /**
  * API Call
  * @param {*} url Noroff api URL
- * @param {*} userData User Data
+ * @param {*} userData
+ * 
+ * ```Use this function to get data from the API``` 
+ * 
  */
 
 async function getUserData(url, userData) {
@@ -14,9 +19,30 @@ async function getUserData(url, userData) {
 
 getUserData()
 
-/*User data*/
+/*Post User data*/
 
-
+fetch('https://jsonplaceholder.typicode.com/posts', {
+  method: 'POST',
+  body: JSON.stringify({
+    "name": "my_username",                          // Required
+    "email": "first.last@stud.noroff.no",           // Required
+    "password": "UzI1NiIsInR5cCI",                  // Required
+    "avatar": "https://img.service.com/avatar.jpg", // Optional
+    "banner": "https://img.service.com/banner.jpg"  // Optional
+  }),
+  headers: {
+    'Content-type': 'application/json; charset=UTF-8',
+  },
+})
+  .then((response) => response.json())
+  .then((json) => console.log(json));
+// Logs:
+// {
+//   id: 101,
+//   title: 'foo',
+//   body: 'bar',
+//   userId: 1
+// }
 
 
 
