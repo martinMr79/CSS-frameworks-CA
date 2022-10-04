7dd8382ba956af2abfb9e385491f472b20c430b6
Martin Mroz <80599295+martinMr79@users.noreply.github.com>
2022-10-04 23:31:49 +0200

    removed js handlers script

--- js/index.mjs
+++ js/index.mjs
@@ -1,4 +1,3 @@
-// import * as constants from "./api/constants.mjs"
-
-// console.log(constants.API_SOCIAL_URL);
+import { setRegisterFormListener } from "./handlers/register.mjs";
  
+setRegisterFormListener()
