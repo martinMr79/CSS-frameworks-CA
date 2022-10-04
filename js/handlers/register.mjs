34dba4f5a3fb98ccd5296217945dbb1efb9a29c9
Martin Mroz <80599295+martinMr79@users.noreply.github.com>
2022-10-04 23:30:59 +0200

    add JSDocs

--- js/handlers/register.mjs
+++ js/handlers/register.mjs
@@ -1,11 +1,16 @@
 
-const form = document.querySelector("#registerForm"); 
+/**
+ * Registers user input as an object when form is submitted 
+ */
+
+export function setRegisterFormListener() {
+    const form = document.querySelector("#registerForm"); 
 
 form.addEventListener("submit", (event) => {
     event.preventDefault()
-
     const form = event.target; 
     const formData = new FormData(form); 
     const profile = Object.fromEntries(formData.entries()) 
-    console.log(profile)
+
 })
+}
