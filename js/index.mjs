37b03d864681197f49d8072e7225172f6d6d3ea2
Martin Mroz <80599295+martinMr79@users.noreply.github.com>
2022-10-07 00:02:44 +0200

    login handler and if/else routing

--- js/index.mjs
+++ js/index.mjs
@@ -1,3 +1,10 @@
 import { setRegisterFormListener } from "./handlers/register.mjs";
- 
-setRegisterFormListener()
+import { setLoginFormListener } from "./handlers/login.mjs"; 
+
+const path = location.pathname;
+
+if (path === '/profile/login/index.html') {
+    setLoginFormListener()
+} else if (path === '/profile/register/index.html') {
+    setRegisterFormListener()
+}
